7e7512a911ff80d1dc84cefbfdaf40e8190237c6
Wouter de Vries <wouter@wouter-web.nl>
2016-07-14 11:10:30 +0200

    Added DNS config

--- Assignments/DNS/dnssec.py
+++ Assignments/DNS/dnssec.py
@@ -16,12 +16,13 @@ class DDoSTopo(Topo):
         attacker = self.addHost('A')
         dnsresolver = self.addHost('B')
         victim = self.addHost('C')
+        capture = self.addHost('D')
 
         #Add links between each of the hosts and the switch
         self.addLink(attacker,switch)
         self.addLink(dnsresolver,switch)
         self.addLink(victim,switch)
-	
+	self.addLink(capture,switch)
 
 def run_exercise():
     #Create and start a new network with our custom topology
@@ -31,21 +32,25 @@ def run_exercise():
 
     #Configure switch so that packets reach the right port (to prevent l2 learning from affecting the exercise)
     net["s1"].dpctl("del-flows")
-    net["s1"].dpctl("add-flow", "dl_type=0x0800,nw_dst=10.0.0.1,actions=output:1")
-    net["s1"].dpctl("add-flow", "dl_type=0x0800,nw_dst=10.0.0.2,actions=output:2")
-    net["s1"].dpctl("add-flow", "dl_type=0x0800,nw_dst=10.0.0.3,actions=output:3")
+    net["s1"].dpctl("add-flow", "dl_type=0x0800,in_port=1,nw_dst=10.0.0.1,actions=output:1,mod_vlan_vid:11,output:4")
+    net["s1"].dpctl("add-flow", "dl_type=0x0800,in_port=1,nw_dst=10.0.0.2,actions=output:2,mod_vlan_vid:11,output:4")
+    net["s1"].dpctl("add-flow", "dl_type=0x0800,in_port=1,nw_dst=10.0.0.3,actions=output:3,mod_vlan_vid:11,output:4")
+    net["s1"].dpctl("add-flow", "dl_type=0x0800,in_port=2,nw_dst=10.0.0.1,actions=output:1,mod_vlan_vid:12,output:4")
+    net["s1"].dpctl("add-flow", "dl_type=0x0800,in_port=2,nw_dst=10.0.0.2,actions=output:2,mod_vlan_vid:12,output:4")
+    net["s1"].dpctl("add-flow", "dl_type=0x0800,in_port=2,nw_dst=10.0.0.3,actions=output:3,mod_vlan_vid:12,output:4")
+    net["s1"].dpctl("add-flow", "dl_type=0x0800,in_port=3,nw_dst=10.0.0.1,actions=output:1,mod_vlan_vid:13,output:4")
+    net["s1"].dpctl("add-flow", "dl_type=0x0800,in_port=3,nw_dst=10.0.0.2,actions=output:2,mod_vlan_vid:13,output:4")
+    net["s1"].dpctl("add-flow", "dl_type=0x0800,in_port=3,nw_dst=10.0.0.3,actions=output:3,mod_vlan_vid:13,output:4")
 
     #Verify connectivity
     net.pingAll()
 
     #Start BIND DNS-server
-    net["B"].popen('named', '-g')
-
- #   makeTerms([net["B"]], title="DNS")	
+    net["B"].popen('named', '-g', '-c', '/home/vagrant/assignments/DNS/config/named.conf')
 
     #Open terminals
     makeTerms([net["A"]], title="Attacker terminal")
-#    makeTerms([net["C"]], title="Victim terminal")
+    makeTerms([net["D"]], title="Capture terminal")
 
 if __name__ == '__main__':
     run_exercise()
